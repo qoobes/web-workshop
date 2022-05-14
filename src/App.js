bf6b0091eceb39c424e3c1eba2a36a00461fe9db
Ahmed Haracic <me@qoobes.space>
2022-05-14 15:08:48 +0200

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { CreateTodo } from "./components/CreateTodo";
 import { TodoList } from "./components/TodoList";
 
 const INITIAL_STATE = [
@@ -18,17 +19,29 @@ function App() {
   const [todos, setTodos] = useState(INITIAL_STATE);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        margin: "10rem",
-        boxShadow: "0px 2px 10px 1px grey",
-        borderRadius: "10px",
-      }}
-    >
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginLeft: "10rem",
+          marginTop: "10rem",
+          marginRight: "10rem",
+          boxShadow: "0px 2px 10px 1px grey",
+          borderRadius: "1rem",
+        }}
+      >
+        <TodoList todos={todos} setTodos={setTodos} />
+      </div>
+      <div
+        style={{
+          marginLeft: "10rem",
+          marginTop: "2rem",
+        }}
+      >
+        <CreateTodo todos={todos} setTodos={setTodos} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,48 @@
+import { useRef } from "react";
+
+export const CreateTodo = ({ todos, setTodos }) => {
+  const nameRef = useRef(null);
+
+  const handleSubmit = () => {
+    const name = nameRef.current.value;
+
+    if (!name || name.length < 2) return alert("Please enter a name");
+
+    setTodos([...todos, { name, complete: false }]);
+  };
+
+  return (
+    <div style={{ width: "100%" }}>
+      {/* Form */}
+      {/* Name of the todo */}
+
+      <input
+        style={{
+          padding: "5px 5px",
+          fontSize: "1.1rem",
+          borderRadius: "5px",
+          outline: "none",
+          border: "1px solid grey",
+          boxShadow: "0px 2px 5px 0px grey",
+        }}
+        type="text"
+        name="name"
+        ref={nameRef}
+      />
+      <button
+        onClick={handleSubmit}
+        style={{
+          padding: "5px 5px",
+          fontSize: "1.1rem",
+          borderRadius: "5px",
+          outline: "none",
+          border: "1px solid grey",
+          boxShadow: "0px 2px 5px 0px grey",
+          cursor: "pointer",
+        }}
+      >
+        Add new todo
+      </button>
+    </div>
+  );
+};
