8aa764c43ca7df0702a645d3793401eaec534e63
Ahmed Haracic <me@qoobes.space>
2022-05-14 14:41:35 +0200

    Fix ordering bug

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,21 +6,23 @@ export const TodoList = ({ todos, setTodos }) => {
 
   return (
     <div style={{ width: "100%" }}>
-      {todos.map((todo, key) => (
-        <div
-          onClick={() => flipComplete(todo)}
-          style={{
-            display: "flex",
-            gap: "1rem",
-            alignItems: "center",
-            backgroundColor: key % 2 === 0 && "#f3f3f3",
-            padding: "1rem",
-          }}
-        >
-          <p>{todo.complete ? "Complete" : "Not Complete"}</p>
-          <p>{todo.name}</p>
-        </div>
-      ))}
+      {todos
+        .sort((a, b) => a.id - b.id)
+        .map((todo, key) => (
+          <div
+            onClick={() => flipComplete(todo)}
+            style={{
+              display: "flex",
+              gap: "1rem",
+              alignItems: "center",
+              backgroundColor: key % 2 === 0 && "#f3f3f3",
+              padding: "1rem",
+            }}
+          >
+            <p>{todo.complete ? "Complete" : "Not Complete"}</p>
+            <p>{todo.name}</p>
+          </div>
+        ))}
     </div>
   );
 };
