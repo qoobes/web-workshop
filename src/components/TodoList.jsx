7e9db9d948497971107cbe409c793c8c2f6a5863
Ahmed Haracic <me@qoobes.space>
2022-05-15 11:27:55 +0200

    Delete button

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,25 +4,45 @@ export const TodoList = ({ todos, setTodos }) => {
     setTodos([...newTodos, { ...todo, complete: !todo.complete }]);
   };
 
+  const deleteTodo = todo => {
+    setTodos(todos.filter(t => t.id !== todo.id));
+  };
+
   return (
     <div style={{ width: "100%" }}>
-      {todos
-        .sort((a, b) => a.id - b.id)
-        .map((todo, key) => (
-          <div
-            onClick={() => flipComplete(todo)}
-            style={{
-              display: "flex",
-              gap: "1rem",
-              alignItems: "center",
-              backgroundColor: key % 2 === 0 && "#f3f3f3",
-              padding: "1rem",
-            }}
-          >
-            <p>{todo.complete ? "Complete" : "Not Complete"}</p>
-            <p>{todo.name}</p>
-          </div>
-        ))}
+      {todos && todos.length > 0 ? (
+        todos
+          .sort((a, b) => a.id - b.id)
+          .map((todo, key) => (
+            <div
+              style={{
+                display: "flex",
+                gap: "1rem",
+                alignItems: "center",
+                justifyContent: "space-between",
+                backgroundColor: key % 2 === 0 && "#f3f3f3",
+                padding: "1rem",
+              }}
+            >
+              <div
+                style={{ display: "flex", gap: "1rem", alignItems: "center" }}
+              >
+                <p>{todo.complete ? "Complete" : "Not Complete"}</p>
+                <p>{todo.name}</p>
+              </div>
+              <div
+                style={{ display: "flex", gap: "1rem", alignItems: "center" }}
+              >
+                <button onClick={() => flipComplete(todo)}>
+                  {todo.complete ? "Set Incomplete" : "Set Complete"}
+                </button>
+                <button onClick={() => deleteTodo(todo)}>Delete</button>
+              </div>
+            </div>
+          ))
+      ) : (
+        <div>No todos, add a new one!</div>
+      )}
     </div>
   );
 };
